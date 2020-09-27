c4b73c9a9b7679150894ff7d6fd661de9e9c303b
Sanjeev Viswanath <sanjeevviswanath@sanjeevs-mbp.lan>
2020-09-27 16:14:25 -0700

    Initial xcode project creation

---
+++ YoutubeApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeApplication
+//
+//  Created by Sanjeev Viswanath on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
